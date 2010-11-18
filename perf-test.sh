f013f18f695e961a835d4d5cfb636c675f6482d4
Raghavendra Bhat <raghavendrabhat@gluster.com>
2010-11-18 17:55:11 +0530

    send the arguments to mkdir through xargs not directly to avoid "Argument list too long error"

--- perf-test.sh
+++ perf-test.sh
@@ -20,9 +20,9 @@ function emptydirs_create()
 {
     mkdir -p $PG/emptydirs;
 
-    eval "mkdir -p $PG/emptydirs/top.{1..$emptytops}";
+    eval "echo $PG/emptydirs/top.{1..$emptytops}" | xargs mkdir -p;
     for top in $(seq 1 $emptytops); do
-        eval "mkdir -p $PG/emptydirs/top.$top/dir.{1..$emptydirs}";
+        eval "echo $PG/emptydirs/top.$top/dir.{1..$emptydirs}" | xargs mkdir -p;
     done
 }
 
@@ -116,7 +116,7 @@ function crawl_create_recurse()
 
     depth=$(($depth - 1));
 
-    eval "mkdir -p $subpath/dir.{1..$crawlwidth}";
+    eval "echo  $subpath/dir.{1..$crawlwidth}" | xargs mkdir -p;
 
     for i in $(seq 1 $crawlwidth); do
 	crawl_create_recurse "$subpath/dir.$i" $depth;
